94dc1531925dc38d7cb51183441cc68cba1ec33b
PoJo93 <jonaspohlmann@yahoo.de>
2017-12-11 18:14:58 +0100

    [#4] - Added FileSaver Class

---
+++ src/main/java/de/hpi/crawler/service/FileSaver.java
@@ -0,0 +1,11 @@
+package de.hpi.crawler.service;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+
+public class FileSaver implements StorageProvider {
+
+    @Override
+    public void storePage(Page page) {
+
+    }
+}
