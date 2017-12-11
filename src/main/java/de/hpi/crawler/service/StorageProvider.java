291a9cbf2715a7d49fcc94bf2f72bb90b490a2f4
PoJo93 <jonaspohlmann@yahoo.de>
2017-12-11 17:46:19 +0100

    Added Interface Storage Provider

---
+++ src/main/java/de/hpi/crawler/service/StorageProvider.java
@@ -0,0 +1,18 @@
+package de.hpi.crawler.service;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+
+/**
+ * This interface is to provide a Storage Provider to save HTML Pages crawled by a crawler
+ * @author jonaspohlmann
+ *
+ */
+
+public interface StorageProvider {
+    /**
+     * @param page A page crawled by the crawler4j
+     */
+
+    public void storePage(Page page);
+
+}
