f9642550ba1bf98cad91025e53521bf30408bc8b
PoJo93 <jonaspohlmann@yahoo.de>
2018-01-08 15:43:15 +0100

    [#4] - Testing if it obeys robots.txt - Removing Maven Dependency (We knew that it obbeyed robots.txt and therefore no need to test)

--- src/test/java/de/hpi/crawler/service/CrawlerServiceTest.java
+++ src/test/java/de/hpi/crawler/service/CrawlerServiceTest.java
@@ -4,21 +4,23 @@ import org.junit.Test;
 
 import static org.mockito.Mockito.spy;
 
+
 public class CrawlerServiceTest {
+
+
 
 
     @Test
     public void crawlDomain() {
         CrawlerService crawlerService = spy(new CrawlerService());
 
-        obeysRobotsTxt(crawlerService);
+        //obeysRobotsTxt(crawlerService);
 
         //doReturn("").when(crawlerService.)
 
         //TODO: Test with a new Storage TestStorage
     }
 
-    private void obeysRobotsTxt(CrawlerService crawlerService) {
 
-    }
+
 }
